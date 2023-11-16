5a574a79466362c572cfd0736e477769a4437891
outofnames <hassandogar9@gmail.com>
2023-11-16 10:59:31 +0100

    Update resume text

--- app/components/modules/AboutMeHero.tsx
+++ app/components/modules/AboutMeHero.tsx
@@ -31,7 +31,7 @@ const AboutMeHero = () => {
           href="https://drive.google.com/file/d/1xbVqjjKushpGkfD84CNqGlouTQd65Wip/view?usp=sharing"
           target="_blank"
           rel="noreferrer">
-          Resume{' '}
+          View CV{' '}
           <div className={styles.icon}>
             <Image src={ArrowImg} alt="arrow" />
           </div>
